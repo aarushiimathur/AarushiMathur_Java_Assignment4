9d916b26e0b12b6b2e19ee9e8e16bf24f192b009
aarushiimathur <148770634+aarushiimathur@users.noreply.github.com>
2025-03-05 10:23:10 +0530

    Card
    Constructor formation

--- Card.java
+++ Card.java
@@ -1,3 +1,9 @@
 class Card {
     private String rank;
     private String suit;
+
+    // Constructor
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
