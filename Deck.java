c913c0a06b0d663a9f1043328533daddc5d7129a
aarushiimathur <148770634+aarushiimathur@users.noreply.github.com>
2025-03-05 10:19:21 +0530

    compare card
    prints all cards of a specific rank

--- Deck.java
+++ Deck.java
@@ -43,4 +43,13 @@ class Deck {
             }
         }
     }
+    // Prints all cards of a specific rank
+    public void compareCard(String rank) {
+        System.out.println("Cards with rank " + rank + ":");
+        for (Card card : deck) {
+            if (card.getRank().equalsIgnoreCase(rank)) {
+                System.out.println(card);
+            }
+        }
+    }
     
