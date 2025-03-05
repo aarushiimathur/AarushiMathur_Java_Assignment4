253a7e53c0e17bd4ef206fb5c68bb0d4be0f2a64
aarushiimathur <148770634+aarushiimathur@users.noreply.github.com>
2025-03-05 10:12:58 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+//Name - Aarushi Mathur
+//PRN - 23070126002
+//AIML - A1
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Menu-driven program for deck operations
+        Scanner scanner = new Scanner(System.in);
+        Deck deck = new Deck();
